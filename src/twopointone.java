190c8878d48de7e766a866398a52258a15b79d4b
Ankith Talpady <ankithtalpady05@gmail.com>
2024-11-30 01:53:06 -0500

    Updated

--- src/twopointone.java
+++ src/twopointone.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 public class twopointone {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Initializing Scanner
+        Scanner celsiusInput = new Scanner(System.in);
+        System.out.println("Enter a degree in Celsius:");
+        float celsiusTemperature = celsiusInput.nextFloat();
+        double fahrenheitValue = (celsiusTemperature * 9.0/5) + 32;
+        System.out.printf("%.2f Celsius is %.2f Fahrenheit%n", celsiusTemperature, fahrenheitValue);
+        System.out.println("Converted Fahrenheit value: " + fahrenheitValue);   
+        celsiusInput.close();
+
         
     }
 }
